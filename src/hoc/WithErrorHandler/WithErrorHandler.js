454fd89176bd3989da5597fb60b18f1a00ce1d02
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-06-17 01:57:22 +0300

    fixed use interceptors.request instead of interceptors.response

--- src/hoc/WithErrorHandler/WithErrorHandler.js
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -8,27 +8,22 @@ const WithErrorHandler = (WrappedComponent, axios) => {
          error: null,
       };
 
-      componentWillMount() {
-         console.log('[WithErrorHandler]:: componentDidMount');
-
+      componentDidMount() {
          this.reqInterceptor = axios.interceptors.request.use(req => {
             // clean the error when calling a new request
             this.setState({ error: null });
             return req;
          });
-         console.log('reqInterceptor:', this.reqInterceptor)
 
          // save the error in any response
          this.resInterceptor = axios.interceptors.response.use(res => res, error => {
             this.setState({ error });
          });
-         console.log('resInterceptor:', this.resInterceptor)
       }
 
       componentWillUnmount() {
-         console.log('[WithErrorHandler]:: componentWillUnmount');
          axios.interceptors.request.eject(this.reqInterceptor);
-         axios.interceptors.request.eject(this.resInterceptor);
+         axios.interceptors.response.eject(this.resInterceptor);
       }
 
       errorConfirmedHandler = () => {
@@ -36,14 +31,13 @@ const WithErrorHandler = (WrappedComponent, axios) => {
       }
 
       render() {
-         console.log('state: ', this.state);
          return (
             <>
-               {/*  <Modal
+               <Modal
                   show={this.state.error ? true : false}
                   closed={this.errorConfirmedHandler}>
                   {this.state.error ? this.state.error.message : null}
-               </Modal> */}
+               </Modal>
                <WrappedComponent {...this.props} />
             </>
          )
