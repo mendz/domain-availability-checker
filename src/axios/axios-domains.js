d8b3996cd73a84376427be05abf4a32d79cad08c
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-06-21 18:20:20 +0300

    update axios to use the base URL according to NODE_ENV

--- src/axios/axios-domains.js
+++ src/axios/axios-domains.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
+console.log(process.env.NODE_ENV);
+
+const getBase = () => {
+   if (process.env.NODE_ENV === 'development') {
+      return 'http://localhost:1598/';
+   }
+   return 'https://domain-info-12547.herokuapp.com/';
+}
+
 const instance = axios.create({
-   // baseURL: 'https://domain-info-12547.herokuapp.com/'
-   baseURL: 'http://localhost:1598/'
+   baseURL: getBase()
 });
 
 export default instance;
