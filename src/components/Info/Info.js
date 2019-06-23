d0c175d72be509887455bf0e27bdd26310fcda18
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-06-23 17:21:18 +0300

    move the DISCLAIMER to the Info modal

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -6,6 +6,7 @@ import classes from './Info.module.css';
 const Info = () => {
    return (
       <div className={classes.Info}>
+         <h1>Legend</h1>
          <p>
             <SymbolsCheck type="success"/> - The domain is available.
          </p>
@@ -14,6 +15,11 @@ const Info = () => {
          </p>
          <p>
             <SymbolsCheck type="error"/> - Something went wrong.
+         </p>
+         <hr />
+         <h2>DISCLAIMER</h2>
+         <p>
+            <small>Currently, the site is using a DNS check to find if the domain has IPs or not, therefore sometimes the result could be false positive.</small>
          </p>
       </div>
    )
