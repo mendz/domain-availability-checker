c1a2eee006527aba419f13769a2cffaf480bda5c
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-07-08 19:14:01 +0300

    change Sentry tracking to be only in production mode

--- src/utils/setupErrorsTrack.js
+++ src/utils/setupErrorsTrack.js
@@ -1,7 +1,13 @@
 import * as Sentry from '@sentry/browser';
+import { version } from '../../package.json';
 
 function initSentry() {
-   Sentry.init({ dsn: "https://6b862531cc6b4c46bad00b3e86a9752f@sentry.io/1498890" });
+   if (process.env.NODE_ENV === 'production') {
+      Sentry.init({
+         dsn: "https://6b862531cc6b4c46bad00b3e86a9752f@sentry.io/1498890",
+         release: version
+      });
+   }
 }
 
 function throwError(event, data) {
