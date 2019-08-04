233bf0ffe474d9763862f90700c25a7987f9f013
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-08-04 10:20:31 -0400

    finished to add button to report

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useToggle = () => {
+   const [on, setOn] = useState(false);
+
+   const toggle = () => {
+      setOn(!on);
+   }
+
+   return [on, toggle];
+}
+
+export default useToggle;
