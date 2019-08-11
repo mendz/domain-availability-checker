e32d51ed3f6a3de46dd9907697ca44d1819ad475
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-08-11 19:49:40 -0400

    fix check to eventId

--- src/utils/setupErrorsTrack.js
+++ src/utils/setupErrorsTrack.js
@@ -4,7 +4,6 @@ import { version } from '../../package.json';
 
 function initSentry() {
    if (isProduction()) {
-      console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
       Sentry.init({
          dsn: "https://6b862531cc6b4c46bad00b3e86a9752f@sentry.io/1498890",
          release: version
@@ -27,9 +26,9 @@ function throwError(event, data) {
 async function reportFeedback() {
    if (isProduction()) {
       const eventId = await Sentry.captureException('User Feedback', () => { });
-      const report = await Sentry.showReportDialog({ eventId });
+      Sentry.showReportDialog({ eventId });
       // if there is no internet connection the report will be undefined
-      if (!report) {
+      if (!eventId) {
          return 'There is no internet connection';
       }
       return true;
