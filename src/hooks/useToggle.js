863a52f02e635135c25dc17c513b6ccbd195b3d1
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-08-26 23:12:37 +0300

    eslint -fix

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 const useToggle = () => {
-   const [on, setOn] = useState(false);
+  const [on, setOn] = useState(false);
 
-   const toggle = () => {
-      setOn(!on);
-   }
+  const toggle = () => {
+    setOn(!on);
+  };
 
-   return [on, toggle];
-}
+  return [on, toggle];
+};
 
 export default useToggle;
