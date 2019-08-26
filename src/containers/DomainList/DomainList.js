af3c3df922dc4ed878edf9f2572c5d746eb7b42e
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-08-26 23:10:57 +0300

    eslint -fix

--- src/containers/DomainList/DomainList.js
+++ src/containers/DomainList/DomainList.js
@@ -10,259 +10,273 @@ import ButtonInfo from '../../components/UniqueButtons/ButtonInfo/ButtonInfo';
 import Info from '../../components/Info/Info';
 
 import axios from '../../axios/axios-domains';
-import { stripDomainFromURL, getDomainFromRequest } from '../../utils/normalizeDomain';
+import {
+  stripDomainFromURL,
+  getDomainFromRequest,
+} from '../../utils/normalizeDomain';
 
 import classes from './DomainList.module.css';
 import Modal from '../../components/UI/Modal/Modal';
 import { throwError } from '../../utils/setupErrorsTrack';
 
 class DomainList extends Component {
-   state = {
+  state = {
+    domainsList: [],
+    decodedDomainList: [],
+    validation: {
+      required: true,
+      isUrl: true,
+      errorMessage: 'Please enter a at least one domain.',
+    },
+    invalidDomains: [],
+    formIsValid: false,
+    checking: false,
+    showInfo: false,
+  };
+  checkValidity = (value, rules) => {
+    let isValid = true;
+
+    // in case the input doesn't have any rules
+    if (!rules) return true;
+
+    if (rules.required) {
+      isValid = value.length !== 0 && isValid;
+    }
+
+    if (rules.isUrl) {
+      const invalidDomains = value.filter(domain => !isFQDN(domain));
+      // .map(domain => domain);
+
+      // if at least one domain is valid we can proceed and check this one,
+      // if all the domain is not valid all the form will be not valid
+      const atLeastOneValid = value.length > invalidDomains.length;
+      isValid = atLeastOneValid && isValid;
+
+      // if at least one domain changed we will update the state
+      if (invalidDomains.length > 0) {
+        this.setState({ invalidDomains });
+      }
+    }
+
+    return isValid;
+  };
+
+  domainListHandle = e => {
+    if (e.target.value === '') {
+      this.setState({ domainsList: [] });
+      return;
+    }
+
+    const domains = e.target.value
+      .split('\n')
+      .map(domain =>
+        /^http.*/.test(domain) ? stripDomainFromURL(domain) : domain
+      );
+
+    const updatedObj = {
+      domainsList: domains,
+      formIsValid: this.checkValidity(domains, this.state.validation),
+    };
+
+    this.setState(prevState => ({
+      ...prevState,
+      ...updatedObj,
+    }));
+  };
+
+  checkDomains = async e => {
+    e.preventDefault();
+    this.setState({ checking: true });
+
+    // update the state with raw information - invalid / loading
+    const decodedDomainListRaw = [...new Set(this.state.domainsList)]
+      .filter(domain => domain.trim() !== '')
+      .map(domain => {
+        const isInvalid = this.state.invalidDomains.find(
+          invalidDomain => invalidDomain === domain
+        );
+        if (!isInvalid) {
+          return {
+            name: domain,
+            availability: null,
+            networkError: false,
+            invalid: false,
+          };
+        } else {
+          return {
+            name: domain,
+            availability: false,
+            networkError: false,
+            invalid: true,
+          };
+        }
+      });
+
+    this.setState({ decodedDomainList: decodedDomainListRaw });
+
+    // start checking the domain and update the state per domain
+    const finishCheckingFiltered = decodedDomainListRaw.filter(
+      domain => !domain.invalid
+    );
+
+    for (const domain of finishCheckingFiltered) {
+      const response = await this.checkAvailable(domain.name);
+      let resultDomain = {};
+
+      // no response due to error, no need to continue check this domain
+      if (!response) {
+        resultDomain = {
+          ...domain,
+          name: domain.name,
+          networkError: true,
+        };
+        this.updateDomainInDecodedDomainList(domain.name, resultDomain);
+        throwError('No response data', resultDomain);
+        continue;
+      }
+
+      const { data, statusText, config } = response;
+
+      if (data && statusText === 'OK') {
+        resultDomain = {
+          ...domain,
+          name: data.domain,
+          availability: data.isAvailable,
+        };
+      } else if (!statusText === 'OK') {
+        resultDomain = {
+          ...domain,
+          name: getDomainFromRequest(config.url),
+          networkError: true,
+        };
+      }
+
+      this.updateDomainInDecodedDomainList(domain.name, resultDomain);
+    }
+
+    // wait for all the checking to finish in order to reopen the textarea
+    this.setState({ checking: false });
+    this.saveToHistory();
+  };
+
+  checkAvailable = async domainName => {
+    // check if debug is on
+    let debug = '';
+    if (process.env.REACT_APP_DEBUG) {
+      debug = '?debug';
+    }
+
+    const response = await axios(`available/${domainName}${debug}`).catch(err =>
+      console.error(`ERROR!!!\n${err}`)
+    );
+    return response;
+  };
+
+  updateDomainInDecodedDomainList = (domainName, resultDomain) => {
+    const updatedDecodedDomainList = [...this.state.decodedDomainList];
+    const index = updatedDecodedDomainList.findIndex(
+      domainToFind => domainToFind.name === domainName
+    );
+    updatedDecodedDomainList[index] = resultDomain;
+
+    this.setState({ decodedDomainList: updatedDecodedDomainList });
+  };
+
+  clearDomains = () => {
+    this.setState({
       domainsList: [],
       decodedDomainList: [],
-      validation: {
-         required: true,
-         isUrl: true,
-         errorMessage: 'Please enter a at least one domain.'
-      },
-      invalidDomains: [],
       formIsValid: false,
-      checking: false,
-      showInfo: false
-   }
-   checkValidity = (value, rules) => {
-      let isValid = true;
+    });
+  };
 
-      // in case the input doesn't have any rules
-      if (!rules) return true;
+  saveToHistory = () => {
+    const oldHistoryString = localStorage.getItem('historyDomains');
+    let currentHistory = '';
 
-      if (rules.required) {
-         isValid = value.length !== 0 && isValid;
-      }
-
-      if (rules.isUrl) {
-         const invalidDomains = value
-            .filter(domain => !isFQDN(domain));
-         // .map(domain => domain);
-
-         // if at least one domain is valid we can proceed and check this one,
-         // if all the domain is not valid all the form will be not valid
-         const atLeastOneValid = value.length > invalidDomains.length;
-         isValid = atLeastOneValid && isValid;
-
-         // if at least one domain changed we will update the state
-         if (invalidDomains.length > 0) {
-            this.setState({ invalidDomains });
-         }
-      }
-
-      return isValid;
-   }
-
-   domainListHandle = (e) => {
-      if (e.target.value === '') {
-         this.setState({ domainsList: [] });
-         return;
-      }
-
-      const domains = e.target.value
-         .split('\n')
-         .map(domain => {
-            return /^http.*/.test(domain) ? stripDomainFromURL(domain) : domain;
-         });
-
-      const updatedObj = {
-         domainsList: domains,
-         formIsValid: this.checkValidity(domains, this.state.validation)
-      }
-
-      this.setState(prevState => ({
-         ...prevState,
-         ...updatedObj
-      }));
-   }
-
-   checkDomains = async e => {
-      e.preventDefault();
-      this.setState({ checking: true });
-
-      // update the state with raw information - invalid / loading
-      const decodedDomainListRaw = [...new Set(this.state.domainsList)]
-         .filter(domain => domain.trim() !== '')
-         .map(domain => {
-            const isInvalid = this.state.invalidDomains.find(invalidDomain => invalidDomain === domain);
-            if (!isInvalid) {
-               return {
-                  name: domain,
-                  availability: null,
-                  networkError: false,
-                  invalid: false
-               }
-            } else {
-               return {
-                  name: domain,
-                  availability: false,
-                  networkError: false,
-                  invalid: true
-               }
-            }
-         });
-
-      this.setState({ decodedDomainList: decodedDomainListRaw });
-
-      // start checking the domain and update the state per domain
-      const finishCheckingFiltered = decodedDomainListRaw
-         .filter(domain => !domain.invalid);
-
-         for (const domain of finishCheckingFiltered) {
-            const response = await this.checkAvailable(domain.name);
-            let resultDomain = {};
-
-            // no response due to error, no need to continue check this domain
-            if (!response) {
-               resultDomain = {
-                  ...domain,
-                  name: domain.name,
-                  networkError: true,
-               };
-               this.updateDomainInDecodedDomainList(domain.name, resultDomain);
-               throwError('No response data', resultDomain);
-               continue;
-            }
-
-            const { data, statusText, config } = response;
-
-            if (data && statusText === 'OK') {
-               resultDomain = {
-                  ...domain,
-                  name: data.domain,
-                  availability: data.isAvailable,
-               }
-            } else if (!statusText === 'OK') {
-               resultDomain = {
-                  ...domain,
-                  name: getDomainFromRequest(config.url),
-                  networkError: true,
-               }
-            }
-
-            this.updateDomainInDecodedDomainList(domain.name, resultDomain);
-         }
-
-      // wait for all the checking to finish in order to reopen the textarea
-      this.setState({ checking: false });
-      this.saveToHistory();
-   }
-
-   checkAvailable = async (domainName) => {
-      // check if debug is on
-      let debug = '';
-      if (process.env.REACT_APP_DEBUG) {
-         debug='?debug';
-      }
-
-      const response = await axios(`available/${domainName}${debug}`)
-         .catch(err => console.error(`ERROR!!!\n${err}`));
-      return response;
-   }
-
-   updateDomainInDecodedDomainList = (domainName, resultDomain) => {
-      const updatedDecodedDomainList = [...this.state.decodedDomainList];
-      const index = updatedDecodedDomainList.findIndex(domainToFind => domainToFind.name === domainName);
-      updatedDecodedDomainList[index] = resultDomain;
-
-      this.setState({ decodedDomainList: updatedDecodedDomainList });
-   }
-
-   clearDomains = () => {
-      this.setState({
-         domainsList: [],
-         decodedDomainList: [],
-         formIsValid: false,
-      });
-   }
-
-   saveToHistory = () => {
-      const oldHistoryString = localStorage.getItem('historyDomains');
-      let currentHistory = '';
-
-      // if there already history it will filter from it the domains that are in the current check
-      // and concat the old history
-      if (oldHistoryString) {
-         const oldHistory = JSON.parse(oldHistoryString);
-         const filteredOld = oldHistory
-            .filter(currentHistory =>
-               !this.state.decodedDomainList.find(domain => domain.name === currentHistory.name));
-         const combineHistory = this.state.decodedDomainList.concat(filteredOld);
-         currentHistory = JSON.stringify(combineHistory);
-      } else {
-         currentHistory = JSON.stringify(this.state.decodedDomainList);
-      }
-
-      localStorage.setItem('historyDomains', currentHistory);
-   }
-
-   goToHistory = () => {
-      this.props.history.push('/history');
-   }
-
-   showInfoModal = () => {
-      this.setState({ showInfo: true });
-   }
-
-   closeInfoModal = () => {
-      this.setState({ showInfo: false });
-   }
-
-   render() {
-      const domains = this.state.domainsList.map(domain => domain).join('\n');
-
-      let domainCheck = null;
-      if (this.state.decodedDomainList.length > 0) {
-         domainCheck = <DomainCheck listDomains={this.state.decodedDomainList} />;
-      }
-      return (
-         <>
-            <div className={classes.HistoryButton}>
-               <Button
-                  clicked={this.goToHistory}
-                  name="go-to-history"
-                  disabled={this.state.checking}
-               >History</Button>
-               <ButtonInfo clicked={this.showInfoModal} />
-               <Modal show={this.state.showInfo} closed={this.closeInfoModal}>
-                  <Info />
-               </Modal>
-            </div>
-            <div className={classes.DomainList}>
-               <Header />
-               <form onSubmit={this.checkDomains}>
-                  <Textarea
-                     change={this.domainListHandle}
-                     value={domains}
-                     readOnly={this.state.checking}
-                     placeholder='List of domain names to check' />
-                  <div className={classes.Buttons}>
-                     <Button
-                        bigger
-                        clicked={this.clearDomains}
-                        type="reset">Clear</Button>
-                     <Button
-                        bigger
-                        type="submit"
-                        disabled={!this.state.formIsValid || this.state.checking}>Check</Button>
-                  </div>
-               </form>
-               {domainCheck}
-            </div>
-         </>
+    // if there already history it will filter from it the domains that are in the current check
+    // and concat the old history
+    if (oldHistoryString) {
+      const oldHistory = JSON.parse(oldHistoryString);
+      const filteredOld = oldHistory.filter(
+        currentHistory =>
+          !this.state.decodedDomainList.find(
+            domain => domain.name === currentHistory.name
+          )
       );
-   }
+      const combineHistory = this.state.decodedDomainList.concat(filteredOld);
+      currentHistory = JSON.stringify(combineHistory);
+    } else {
+      currentHistory = JSON.stringify(this.state.decodedDomainList);
+    }
+
+    localStorage.setItem('historyDomains', currentHistory);
+  };
+
+  goToHistory = () => {
+    this.props.history.push('/history');
+  };
+
+  showInfoModal = () => {
+    this.setState({ showInfo: true });
+  };
+
+  closeInfoModal = () => {
+    this.setState({ showInfo: false });
+  };
+
+  render() {
+    const domains = this.state.domainsList.map(domain => domain).join('\n');
+
+    let domainCheck = null;
+    if (this.state.decodedDomainList.length > 0) {
+      domainCheck = <DomainCheck listDomains={this.state.decodedDomainList} />;
+    }
+    return (
+      <>
+        <div className={classes.HistoryButton}>
+          <Button
+            clicked={this.goToHistory}
+            name="go-to-history"
+            disabled={this.state.checking}
+          >
+            History
+          </Button>
+          <ButtonInfo clicked={this.showInfoModal} />
+          <Modal show={this.state.showInfo} closed={this.closeInfoModal}>
+            <Info />
+          </Modal>
+        </div>
+        <div className={classes.DomainList}>
+          <Header />
+          <form onSubmit={this.checkDomains}>
+            <Textarea
+              change={this.domainListHandle}
+              value={domains}
+              readOnly={this.state.checking}
+              placeholder="List of domain names to check"
+            />
+            <div className={classes.Buttons}>
+              <Button bigger clicked={this.clearDomains} type="reset">
+                Clear
+              </Button>
+              <Button
+                bigger
+                type="submit"
+                disabled={!this.state.formIsValid || this.state.checking}
+              >
+                Check
+              </Button>
+            </div>
+          </form>
+          {domainCheck}
+        </div>
+      </>
+    );
+  }
 }
 
 export default withErrorHandler(DomainList, axios);
 
 // for testing without the HOC
-export {
-   DomainList
-}
+export { DomainList };
