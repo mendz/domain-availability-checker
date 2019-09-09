3943c92d1269551462879d66b5b3dea9a783fcd2
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-09-09 16:59:49 +0300

    run eslint -fix

--- src/__tests__/DomainCheck.test.js
+++ src/__tests__/DomainCheck.test.js
@@ -6,23 +6,35 @@ import DomainCheck from '../components/DomainCheck/DomainCheck';
 import DomainCheckItem from '../components/DomainCheck/DomainCheckItem/DomainCheckItem';
 
 const mockDecodeDomainListArr = [
-   { name: "test", availability: false, networkError: false, invalid: true },
-   { name: "www.w3schools", availability: false, networkError: false, invalid: true },
-   { name: "github.com", availability: false, networkError: false, invalid: false }
+  { name: 'test', availability: false, networkError: false, invalid: true },
+  {
+    name: 'www.w3schools',
+    availability: false,
+    networkError: false,
+    invalid: true,
+  },
+  {
+    name: 'github.com',
+    availability: false,
+    networkError: false,
+    invalid: false,
+  },
 ];
 
 describe('<DomainCheck />', () => {
-   let wrapper;
+  let wrapper;
 
-   beforeEach(() => {
-      wrapper = mount(<DomainCheck listDomains={mockDecodeDomainListArr} />);
-   });
+  beforeEach(() => {
+    wrapper = mount(<DomainCheck listDomains={mockDecodeDomainListArr} />);
+  });
 
-   it('matches the snapshot', () => {
-      expect(toJSON(wrapper)).toMatchSnapshot();
-   });
+  it('matches the snapshot', () => {
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
 
-   it('should have the exact number of items as state.listDomains length', () => {
-      expect(wrapper.find(DomainCheckItem)).toHaveLength(wrapper.props().listDomains.length);
-   });
+  it('should have the exact number of items as state.listDomains length', () => {
+    expect(wrapper.find(DomainCheckItem)).toHaveLength(
+      wrapper.props().listDomains.length
+    );
+  });
 });
--- src/__tests__/normalizeDomain.test.js
+++ src/__tests__/normalizeDomain.test.js
@@ -1,24 +1,29 @@
-import { stripDomainFromURL, getDomainFromRequest } from '../utils/normalizeDomain';
+import {
+  stripDomainFromURL,
+  getDomainFromRequest,
+} from '../utils/normalizeDomain';
 import { DEV_API, PROD_API } from '../axios/axios-domains';
 
 describe('normalize domain functions', () => {
-   it('should return domain without dots or http', () => {
-      const mockLongUrl = 'https://jestjs.io/docs/en/tutorial-react';
-      const domainOfLongUrl = stripDomainFromURL(mockLongUrl);
-      expect(domainOfLongUrl).toBe('jestjs.io');
+  it('should return domain without dots or http', () => {
+    const mockLongUrl = 'https://jestjs.io/docs/en/tutorial-react';
+    const domainOfLongUrl = stripDomainFromURL(mockLongUrl);
+    expect(domainOfLongUrl).toBe('jestjs.io');
 
-      const mockShortUrl = 'https://github.com/';
-      const domainOfShortUrl = stripDomainFromURL(mockShortUrl);
-      expect(domainOfShortUrl).toBe('github.com');
-   });
+    const mockShortUrl = 'https://github.com/';
+    const domainOfShortUrl = stripDomainFromURL(mockShortUrl);
+    expect(domainOfShortUrl).toBe('github.com');
+  });
 
-   it('should return the domain name from the API get request', () => {
-      const mockEndpoint = 'available/github.com';
+  it('should return the domain name from the API get request', () => {
+    const mockEndpoint = 'available/github.com';
 
-      const domainFromDevCall = getDomainFromRequest(`${DEV_API}${mockEndpoint}`);
-      expect(domainFromDevCall).toBe('github.com');
+    const domainFromDevCall = getDomainFromRequest(`${DEV_API}${mockEndpoint}`);
+    expect(domainFromDevCall).toBe('github.com');
 
-      const domainFromProdCall = getDomainFromRequest(`${PROD_API}${mockEndpoint}`);
-      expect(domainFromProdCall).toBe('github.com');
-   });
+    const domainFromProdCall = getDomainFromRequest(
+      `${PROD_API}${mockEndpoint}`
+    );
+    expect(domainFromProdCall).toBe('github.com');
+  });
 });
