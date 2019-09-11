b520621de98ff6a07b9e30e3aec08190b75566e5
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-09-11 13:16:01 +0300

    add/fix tests for HistoryDomins.test.js

--- src/__tests__/store/reducers/historyDomains.test.js
+++ src/__tests__/store/reducers/historyDomains.test.js
@@ -201,4 +201,75 @@ describe('history domains reducer', () => {
   });
 
   // TODO: add test for filter by the text: HISTORY_DOMAINS_SET_FILTERED, probably will need to move the filter to the reducer and change the sorted to string
+
+  //   it('input search will change filteredDomains to the domains that the search includes', () => {
+  //     // set input search value
+  //     wrapper.find('input').simulate('change', { target: { value: 's' } });
+  //     expect(wrapper.state().historyDomains).toEqual(mockDomains);
+  //     expect(wrapper.state().filteredDomains).toEqual([
+  //       {
+  //         name: 'stackoverflow.com',
+  //         availability: false,
+  //         networkError: false,
+  //         invalid: false,
+  //       },
+  //       {
+  //         name: 'www.w3schools.com',
+  //         availability: false,
+  //         networkError: false,
+  //         invalid: false,
+  //       },
+  //     ]);
+  //   });
+
+  //   it('should filter the domains using the search input with the sorted buttons', () => {
+  //     // 1. set input search value
+  //     wrapper.find('input').simulate('change', { target: { value: 'me' } });
+  //     // 2. filter the domains by the fail domains
+  //     wrapper
+  //       .find(Button)
+  //       .findWhere(comp => comp.prop('name') === 'show-fail')
+  //       .props()
+  //       .clicked();
+  //     // 3. the filterer domains should be only one
+  //     expect(wrapper.state().filteredDomains).toEqual([
+  //       {
+  //         name: 'mendy',
+  //         availability: false,
+  //         networkError: false,
+  //         invalid: true,
+  //       },
+  //     ]);
+  //     // 4. changing the search input will filter from the sorted domains
+  //     wrapper.find('input').simulate('change', { target: { value: 'me' } });
+  //     // the filterer domains should be the same
+  //     expect(wrapper.state().filteredDomains).toEqual([
+  //       {
+  //         name: 'mendy',
+  //         availability: false,
+  //         networkError: false,
+  //         invalid: true,
+  //       },
+  //     ]);
+  //     // 5. changing the sort button will filter the domains by the search value if exists
+  //     wrapper
+  //       .find(Button)
+  //       .findWhere(comp => comp.prop('name') === 'show-success')
+  //       .props()
+  //       .clicked();
+  //     expect(wrapper.state().filteredDomains).toEqual([
+  //       {
+  //         name: 'mendy22323.com',
+  //         availability: true,
+  //         networkError: false,
+  //         invalid: false,
+  //       },
+  //       {
+  //         name: 'mendy2323232323.co.il',
+  //         availability: true,
+  //         networkError: false,
+  //         invalid: false,
+  //       },
+  //     ]);
+  //   });
 });
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types';
 
 import classes from './Button.module.css';
 
+// TODO: remove clicked to use props.onClick
 const Button = ({ clicked, bigger, className, ...props }) => {
-   const classArr = [classes.Button, className];
+  const classArr = [classes.Button, className];
 
-   if (bigger) {
-      classArr.push(classes.Bigger);
-   }
+  if (bigger) {
+    classArr.push(classes.Bigger);
+  }
 
-   return (
-      <button
-         className={classArr.join(' ')}
-         onClick={clicked}
-         {...props}>{props.children}</button>
-   );
+  return (
+    <button className={classArr.join(' ')} onClick={clicked} {...props}>
+      {props.children}
+    </button>
+  );
 };
 
 Button.propTypes = {
-   clicked: PropTypes.func,
-   bigger: PropTypes.bool,
-   className: PropTypes.string,
-}
+  clicked: PropTypes.func,
+  bigger: PropTypes.bool,
+  className: PropTypes.string,
+};
 
-export default Button
+export default Button;
