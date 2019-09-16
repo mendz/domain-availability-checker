bcc69912012038ce4c5f42e0c57f9dbdc9f1ac1a
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-09-16 19:01:32 +0300

    run eslint -fix

--- src/components/UI/Textarea/Textarea.js
+++ src/components/UI/Textarea/Textarea.js
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 import classes from './Textarea.module.css';
 
 const Textarea = ({ change, value, ...props }) => (
-   <textarea
-      className={classes.Textarea}
-      onChange={change}
-      value={value}
-      {...props} />
+  <textarea
+    className={classes.Textarea}
+    onChange={change}
+    value={value}
+    {...props}
+  />
 );
 
 Textarea.propTypes = {
-   change: PropTypes.func.isRequired,
-   value: PropTypes.string.isRequired,
-}
+  change: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default Textarea;
