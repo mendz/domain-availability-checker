003baf9f7aa96aabbce9f9997318e51e899a6f90
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-11-19 01:54:32 +0200

    fix modal position - need to fix z-index issue

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,40 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CSSTransition from 'react-transition-group/CSSTransition';
+import styled, { css } from 'styled-components/macro';
 
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.module.css';
 
 const animationTimeout = {
-   enter: 400,
-   exit: 1000
-}
+  enter: 400,
+  exit: 1000,
+};
 
-const Modal = ({ show, closed, children }) => {
-   return (
-      <>
-         {show && <Backdrop closed={closed} />}
-         <CSSTransition
-            mountOnEnter
-            unmountOnExit
-            in={show}
-            timeout={animationTimeout}
-            classNames={{
-               enterActive: classes.ModalOpen,
-               exitActive: classes.ModalClose
-            }}>
-            <div className={classes.Modal}>
-               {children}
-            </div>
-         </CSSTransition>
-      </>
-   );
-}
+const Modal = ({ show, closed, children }) => (
+  <ModalMainWrapper show={show}>
+    {show && <Backdrop closed={closed} />}
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={show}
+      timeout={animationTimeout}
+      classNames={{
+        enterActive: classes.ModalOpen,
+        exitActive: classes.ModalClose,
+      }}
+    >
+      <ModalContentWrapper>
+        <div className={classes.Modal}>{children}</div>
+      </ModalContentWrapper>
+    </CSSTransition>
+  </ModalMainWrapper>
+);
 
 Modal.propTypes = {
-   show: PropTypes.bool.isRequired,
-   closed: PropTypes.func.isRequired,
-}
+  show: PropTypes.bool.isRequired,
+  closed: PropTypes.func.isRequired,
+};
+
+const ModalMainWrapper = styled.div`
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 501;
+  ${props =>
+    !props.show &&
+    css`
+      display: none;
+    `};
+`;
+
+const ModalContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  max-width: 100%;
+  width: 100%;
+  height: 100%;
+`;
 
 export default Modal;
